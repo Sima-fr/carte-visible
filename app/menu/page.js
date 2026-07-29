9d639ba6bc7001b0ad5f8ca8f8ea89b838fa82c9
Sima-fr <sima.sargsuan@icloud.com>
2026-07-29 20:49:03 +0200

    Change Facebook icon SVG in SocialIcon component
    Updated the Facebook SVG icon in the SocialIcon component to a new design.

--- app/menu/page.js
+++ app/menu/page.js
@@ -341,11 +341,10 @@ function CategoryLevel({ parentId, depth, byParent, dishesByCat, settings, dishe
 
 function SocialIcon({ type }) {
   const common = { width: 20, height: 20, viewBox: '0 0 24 24', fill: 'none', stroke: 'currentColor', strokeWidth: 1.8, strokeLinecap: 'round', strokeLinejoin: 'round' };
-  if (type === 'facebook') {
+if (type === 'facebook') {
     return (
-      <svg {...common}>
-        <path d="M15 8.5h-2a1.5 1.5 0 0 0-1.5 1.5v2H15l-.4 3h-2.1v7" fill="none" />
-        <path d="M9 13.5h6" />
+      <svg width="20" height="20" viewBox="0 0 320 512" fill="currentColor">
+        <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
       </svg>
     );
   }
